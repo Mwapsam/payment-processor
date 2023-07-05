742f52f675b23f2fe6856db7f659978e969bb87b
Mwapsam <mwapsam@gmail.com>
2023-07-05 19:30:50 +0200

    Validate PaymentMethed fields

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,7 +1,11 @@
 class PaymentMethod < ApplicationRecord
-    has_many :transactions
+    has_one :fee, dependent: :destroy
+    has_many :transactions, dependent: :destroy
+    
     validates :payment_method_type, presence: true
     validates :payment_method_name, presence: true, uniqueness: true
     validates :fee_structure, presence: true
+
+    has_one_attached :image
 end
   
