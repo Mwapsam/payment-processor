039c6d443ec5df82da2527235e35743118305f89
Mwapsam <mwapsam@gmail.com>
2023-07-05 19:30:05 +0200

    Validate fees fields

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,6 +1,6 @@
 class Fee < ApplicationRecord
     belongs_to :payment_method
-    validates :transaction_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+    validates :payment_method_id, uniqueness: true
     validates :fee_percentage, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
-  
