f8a12302abea40ccdae08c6370b5acd913c96962
Mwapsam <mwapsam@gmail.com>
2023-07-05 19:28:32 +0200

    Create the users API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :authenticate_user, only: [:index]
+    before_action :authenticate_user, only: [:index, :signed_user]
 
     def index
         users = User.all
         render json: users, status: 201
+    end
+
+    def signed_user
+      render json: @current_user, status: 201
     end
 
     def create
@@ -11,19 +15,19 @@ class Api::V1::UsersController < ApplicationController
     
         if user.save
           token = issue_token(user)
-          render json: { user: user, jwt: token}
+          render json: { user: user, jwt: token}, status: :created
     
         elsif user.errors.messages
-          render json: { errors: user.errors.messages }
+          render json: user.errors.full_messages.to_sentence, status: :unprocessable_entity
         else
-          render json: { error: 'User could not be created.' }
+          render json: { error: 'User could not be created.' }, status: :internal_server_error
         end
     end
     
     private
 
     def user_params
-        # params.require(:user).permit(:name, :phone_number, :email, :password)
-        params.permit(:name, :phone_number, :email, :password)
+        params.require(:user).permit(:name, :phone_number, :email, :password)
+        # params.permit(:name, :phone_number, :email, :password)
     end
 end
