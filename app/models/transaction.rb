a5302829d2e6dd050f4af888db2907f9dcab311d
Mwapsam <mwapsam@gmail.com>
2023-07-05 19:33:16 +0200

    Validate transaction fields

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,9 +1,16 @@
 class Transaction < ApplicationRecord
     belongs_to :user
     belongs_to :payment_method
+  
     validates :transaction_status, presence: true
     validates :transaction_amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :transaction_id, presence: true, uniqueness: true
     validates :transaction_timestamp, presence: true
+  
+    after_create :update_user_balance
+  
+    def update_user_balance
+      user.update_balance(transaction_amount)
+    end
 end
   
